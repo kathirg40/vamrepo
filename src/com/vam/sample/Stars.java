d23e2df320c5ecf1458acc2ece7febffcd289b0c
kathir <kathirg@vamsoft.in>
2016-01-07 20:50:25 +0530

    added %method

--- src/com/vam/sample/Stars.java
+++ src/com/vam/sample/Stars.java
@@ -12,9 +12,19 @@ public void PrintStars() {
       }
       System.out.println(); // Go to next line
     }
+}
 	
-	
-	
+    public void PrintPercentage() {
+    	
+    	int row, numberOfStars;
+    	 
+        for (row = 1; row <= 10; row++) {
+          for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
+            System.out.print("%");
+          }
+          System.out.println(); // Go to next line
+        }
+    	
 	
 }
 
--- src/com/vam/sample/StarsMain.java
+++ src/com/vam/sample/StarsMain.java
@@ -5,7 +5,7 @@ public class StarsMain {
 	public static void main(String[] args) {
 		Stars sample1=new Stars();
 		sample1.PrintStars();
-		
+		sample1.PrintPercentage();
 
 	}
 
