28de0e573f7a3ad6cad551dc4ba9aa216631b099
vairamuthu <vairamuthu@vamsoft.in>
2016-01-08 12:30:45 +0530

    Added $ method in user 2

--- src/com/vam/sample/Stars.java
+++ src/com/vam/sample/Stars.java
@@ -27,6 +27,19 @@ public void printPercentage() {
       System.out.println(); // Go to next line
     }
 }
+
+public void printDollor() {
+	
+	int row, numberOfStars;
+	 
+    for (row = 1; row <= 10; row++) {
+      for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
+        System.out.print("$");
+      }
+      System.out.println(); // Go to next line
+      System.out.println(); // Go to next line
+    }
+}
       
    
 }
