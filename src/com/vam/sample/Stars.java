531e78fa88e155bff1948eb9391dc3bcf213c3ec
vairamuthu <vairamuthu@vamsoft.in>
2016-01-08 12:17:16 +0530

    added % method

--- src/com/vam/sample/Stars.java
+++ src/com/vam/sample/Stars.java
@@ -14,6 +14,19 @@ public void printStars() {
       System.out.println(); // Go to next line
     }
 }
+
+public void printPercentage() {
+	
+	int row, numberOfStars;
+	 
+    for (row = 1; row <= 10; row++) {
+      for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
+        System.out.print("%");
+      }
+      System.out.println(); // Go to next line
+      System.out.println(); // Go to next line
+    }
+}
       
    
 }
