d3cc34a13e705dd28fb3dd2a474754ce5e046f96
vairamuthu <vairamuthu@vamsoft.in>
2016-01-08 15:45:02 +0530

    deleted

--- src/com/vam/sample/Stars.java
+++ src/com/vam/sample/Stars.java
@@ -11,51 +11,8 @@ public void printStars() {
         System.out.print("*");
       }
       System.out.println(); // Go to next line
-      System.out.println(); // Go to next line
-    }
-}
-
-public void printPercentage() {
-	
-	int row, numberOfStars;
-	 
-    for (row = 1; row <= 10; row++) {
-      for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
-        System.out.print("%");
-      }
-      System.out.println("hello"); // Go to next line
-      System.out.println(); // Go to next line
-    }
-}
-
-public void printDollor() {
-	
-	int row, numberOfStars;
-	 
-    for (row = 1; row <= 10; row++) {
-      for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
-        System.out.print("$");
-      }
-      System.out.println("hai"); // Go to next line
-      System.out.println(); // Go to next line
-    }
-}
      
-private void syso() {
-	
-
+    }
 }
 
-public void printAnd() {
-	
-	int row, numberOfStars;
-	 
-    for (row = 1; row <= 10; row++) {
-      for(numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
-        System.out.print("$");
-      }
-      System.out.println(); // Go to next linesss
-      System.out.println(); // Go to next linesss
-    }
-
-}}
+}
